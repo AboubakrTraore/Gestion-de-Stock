de0a49990a5be2ad6aff59f1e77e2658426f5d15
AboubakrTraore <abukartraore@gmail..com>
2025-10-21 16:52:03 +0000

    Configuration de l'environnement de travail

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const db = require('./config/config');
+
+dotenv.config();
+
+//Initialisation de l'application Express
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+//Middleware pour parser le JSON
+app.use(express.json());
+
+//Middleware CORS
+app.use(cors());
+
+//Lancer le serveur
+app.listen(PORT, async () => {
+
+    //Vérification de la connexion à la base de données
+    try {
+        await db.authenticate();    
+        console.log('Connexion à la base de données réussies.');
+    } catch (error) {
+        console.error('Impossible de se connecter à la base de données :', error);
+    }  
+    console.log(`Le serveur est en cours d'exécution sur le port ${PORT}`);
+});
---
+++ models/user.model.js
@@ -0,0 +1,9 @@
+// models/user.model.js
+const {DataTypes} = require('sequelize');// Importation de DataTypes depuis Sequelize
+const db = require('../config/config');// Importation de la configuration de la base de données
+const bcrypt = require('bcrypt');// Importation de bcrypt pour le hachage des mots de passe
+
+// Définition du modèle User    
+
+
+
